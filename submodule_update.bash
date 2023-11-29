21057655380bb1b35c8ed923dc33ea13102624ef
nc <nikita@nodarsensor.com>
2023-11-29 16:29:13 +0100

    Fixed the bug when the submodule was not pushed.

--- submodule_update.bash
+++ submodule_update.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Update all submodules
+# pull all submodules
 git submodule foreach --recursive '
     branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch);
     git fetch origin &&
     git checkout $branch &&
-    git pull origin $branch &&
+    git pull origin $branch
+'
+
+# push all submodules
+git submodule foreach --recursive '
+    branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch);
     if ! git diff --quiet; then
         git add . &&
         git commit -m "Update submodule $name to latest $branch" &&

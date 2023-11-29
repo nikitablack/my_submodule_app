f1f132c078f2d0c6e5d6b2f9f12f7800724514fc
nc <nikita@nodarsensor.com>
2023-11-29 16:18:14 +0100

    Added script for submodules pull/push.

---
+++ submodule_update.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update all submodules
+git submodule foreach '
+    branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch);
+    git fetch origin &&
+    git checkout $branch &&
+    git pull origin $branch &&
+    if ! git diff --quiet; then
+        git add . &&
+        git commit -m "Update submodule $name to latest $branch" &&
+        git push origin $branch
+    fi
+'
